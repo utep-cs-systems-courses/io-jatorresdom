18acc0413b8b989407a8de6924f1fef74bc572f0
jatorresdom <jatorresdom@miners.utep.edu>
2023-11-08 15:10:44 -0700

    Sugested activity complete

--- demos/2-led-interrupt/main.c
+++ demos/2-led-interrupt/main.c
@@ -3,6 +3,8 @@
 #include "libTimer.h"
 #include "led.h"
 
+static unsigned int led_state = 0;
+
 int main(void) {
   P1DIR |= LEDS;
   P1OUT &= ~LED_GREEN;
@@ -18,6 +20,58 @@ int main(void) {
 void
 __interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
 {
-  P1OUT |= LED_GREEN;
+  static int blink_count = 0; // This will control the speed of blinking to make the LED appear dim
+
+
+
+  // Toggle the LED state based on blink_count
+
+  if (++blink_count == 500) { // Adjust this value for LED brightness (lower is brighter)
+
+    blink_count = 0;          // Reset counter
+
+    led_state++;              // Move to the next state
+
+
+
+    if (led_state > 3) {
+
+      led_state = 0; // Reset state if it exceeds the number of states we have
+
+    }
+
+
+
+    // Update LEDs based on the state
+
+    switch (led_state) {
+
+    case 0: // All LEDs off
+
+      P1OUT &= ~LEDS;
+
+      break;
+
+    case 1: // Only Green LED on
+
+      P1OUT = (P1OUT & ~LED_RED) | LED_GREEN;
+
+      break;
+
+    case 2: // Only Red LED on
+
+      P1OUT = (P1OUT & ~LED_GREEN) | LED_RED;
+
+      break;
+
+    case 3: // All LEDs on
+
+      P1OUT |= LEDS;
+
+      break;
+
+    }
+
+  }
 } 
 
